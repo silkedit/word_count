ffee348d7b1f11a6f0f140c8d5e5694d3a104d4f
Shinichi Katayama <shin1.katayama@gmail.com>
2016-03-17 12:10:37 +0900

    Fix tr function arguments

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ module.exports = {
 	},
 
 	commands: {
-		"word_count": function() {
+		'word_count': function() {
 			var text = silkedit.App.activeTextEditView().text
 			if (text !== undefined) {
-				var count = text.split(" ").filter(function(elem){ return elem !== ""; }).length
-				silkedit.alert(silkedit.tr("word_count:word_count", "word count") + ": " + count)
+				var count = text.split(' ').filter(function(elem){ return elem !== ''; }).length
+				silkedit.alert(silkedit.tr('word_count', 'word_count', 'word count') + ': ' + count)
 			} else {
-				console.log("text is undefined")
+				console.log('text is undefined')
 			}
 		}
 	}
